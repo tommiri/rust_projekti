0b3fa45ff1343c83bbb811d24e091f73f0d95519
Henri Sjöblom <henri.sjoblom@outlook.com>
2024-11-21 21:19:13 +0200

    added routing

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+  return (
+    <>
+      <h1 className="page__header">Oops, something went wrong!</h1>
+      <p className="page__text">Not finding that page you are looking for.</p>
+    </>
+  )
+}
+
+export default ErrorPage;
