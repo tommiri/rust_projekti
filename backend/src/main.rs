56887685c4c305535eb3d82e57fe77cca02fbd82
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-11-28 12:21:30 +0200

    Mount routes with db connected

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,25 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+mod db;
+mod jwt;
+mod models;
+mod routes;
+mod schema;
+
+use crate::db::init_pool;
+use dotenvy::dotenv;
+use std::env;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_pool = init_pool(&database_url);
+
+    rocket::build().manage(db_pool).mount(
+        "/",
+        routes![routes::register, routes::login, routes::protected],
+    )
 }
