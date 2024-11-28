2c7e8565f6524e0447474f555f811258b8f1c410
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-11-28 12:14:42 +0200

    Modified table structure

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` VARCHAR(36) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `first_name` VARCHAR(50),
-  `last_name` VARCHAR(50),
-  `password` VARCHAR(60) NOT NULL,
+  `id` VARCHAR(255) NOT NULL UNIQUE,
+  -- UUID
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `first_name` VARCHAR(255),
+  `last_name` VARCHAR(255),
+  `password_hash` VARCHAR(255) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`id`)
@@ -11,24 +12,30 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 CREATE TABLE IF NOT EXISTS `emails` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `user` VARCHAR(36) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
+  `user` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`id`),
-  FOREIGN KEY(`user`) REFERENCES `users`(`id`)
-  ON DELETE CASCADE
+  FOREIGN KEY(`user`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
-INSERT INTO `users` (`id`, `email`, `password`)
-VALUES (
-  '1',
-  'db@user.com',
-  'test'
-);
+INSERT INTO
+  `users` (`id`, `email`, `password`)
+VALUES
+  (1, 'db@user.com', 'test');
 
-INSERT INTO `emails` (`user`, `email`)
-VALUES (
-  (SELECT `id` FROM `users` WHERE `email` = 'db@user.com'),
-  'db@test.com'
-);
+INSERT INTO
+  `emails` (`user`, `email`)
+VALUES
+  (
+    (
+      SELECT
+        `id`
+      FROM
+        `users`
+      WHERE
+        `email` = 'db@user.com'
+    ),
+    'db@test.com'
+  );
