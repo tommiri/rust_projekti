51180f8e19cc11ec971eac10c565279a2a58b9e3
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-12-11 22:01:18 +0200

    Moved models to own module

---
+++ backend/src/models/user.rs
@@ -0,0 +1,28 @@
+use crate::db::schema::users;
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::db::schema::users)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
+pub struct User {
+    pub id: Vec<u8>, // BINARY(16) for UUID
+    pub email: String,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub password_hash: String,
+    pub email_verified: bool,
+    pub verification_token: Option<String>,
+    pub verification_expires: Option<NaiveDateTime>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub email: &'a str,
+    pub password_hash: &'a str,
+    pub verification_token: Option<&'a str>,
+    pub verification_expires: Option<NaiveDateTime>,
+}
