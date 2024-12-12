4a6fdad6b8b804e4d82a45345428e49ccef08d88
Henri Sjöblom <henri.sjoblom@outlook.com>
2024-12-12 14:15:55 +0200

    health route updated

--- backend/src/api/health.rs
+++ backend/src/api/health.rs
@@ -1,9 +1,9 @@
-#[derive(Serialize)]
-pub struct HealthStatus {
-    status: &'static str,
-}
+use serde::Serialize;
+use rocket::{get, serde::json::Json, Request};
+use rocket::http::Status;
+
 
 #[get("/health")]
-pub async fn health() -> Json<HealthStatus> {
-    Json(HealthStatus { status: "OK" })
+pub fn health() -> Result<Json<String>, Status> {
+    Ok(Json("Ok".to_string()))
 }
