a88d5e6c6a16a3f89e259cfbba5b13712fb60d48
Henri Sjöblom <henri.sjoblom@outlook.com>
2024-12-17 12:42:15 +0200

    loading spinner added to registerform

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -12,7 +13,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import register from '@/services/register';
-import { useAuth } from '@/providers/authProvider';
+import { LoadingSpinner } from './LoadingSpinner';
 
 const FormSchema = z.object({
   firstName: z.string().min(1, 'Etunimi on pakollinen'),
@@ -26,7 +27,7 @@ const FormSchema = z.object({
 
 const RegisterForm = () => {
   const navigate = useNavigate();
-  const auth = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm({
     resolver: zodResolver(FormSchema),
@@ -39,93 +40,105 @@ const RegisterForm = () => {
   });
 
   const handleRegister = async (values) => {
+    setIsLoading(true);
     const firstName = values.firstName;
     const lastName = values.lastName;
     const email = values.email;
     const password = values.password;
-    const data = await register(firstName, lastName, email, password);
-    navigate(`/verify/${encodeURIComponent(email)}`);
+    try {
+      await register(firstName, lastName, email, password);
+      navigate(`/verify/${encodeURIComponent(email)}`);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleRegister)}>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <FormField
-                control={form.control}
-                name="firstName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="firstName">Etunimi</FormLabel>
-                    <FormControl>
-                      <Input id="firstName" {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+    <>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleRegister)}>
+            <div className="grid gap-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="firstName">Etunimi</FormLabel>
+                        <FormControl>
+                          <Input id="firstName" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="lastName">Sukunimi</FormLabel>
+                        <FormControl>
+                          <Input id="lastName" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="email">Sähköposti</FormLabel>
+                      <FormControl>
+                        <Input id="email" type="email" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="password">Salasana</FormLabel>
+                      <FormControl>
+                        <Input
+                          id="password"
+                          type="password"
+                          autoComplete="on"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Luo tili
+              </Button>
             </div>
-            <div className="grid gap-2">
-              <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="lastName">Sukunimi</FormLabel>
-                    <FormControl>
-                      <Input id="lastName" {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+            <div className="mt-4 text-center text-sm">
+              Onko sinulla jo tili?{' '}
+              <Link to="/login" className="underline">
+                Kirjaudu sisään
+              </Link>
             </div>
-          </div>
-          <div className="grid gap-2">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="email">Sähköposti</FormLabel>
-                  <FormControl>
-                    <Input id="email" type="email" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="grid gap-2">
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="password">Salasana</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="password"
-                      type="password"
-                      autoComplete="on"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button type="submit" className="w-full">
-            Luo tili
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Onko sinulla jo tili?{' '}
-          <Link to="/login" className="underline">
-            Kirjaudu sisään
-          </Link>
-        </div>
-      </form>
-    </Form>
+          </form>
+        </Form>
+      )}
+    </>
   );
 };
 
